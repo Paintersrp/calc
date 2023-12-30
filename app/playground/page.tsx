c4efe22756106f7ac6758757ab2e69b41692cedb
Ryan Painter <paintersrp@gmail.com>
2023-12-30 07:32:38 -0500

    ^

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,13 +1,34 @@
 import type { FC } from "react"
 
-interface PageNameProps {
-  // Add your prop types here
-}
+const PageName: FC = ({}) => {
+  const data = [
+    { name: "Item 1", quantity: 5, price: 10 },
+    { name: "Item 2", quantity: 3, price: 15 },
+    { name: "Item 3", quantity: 2, price: 8 },
+  ]
 
-const PageName: FC<PageNameProps> = ({}) => {
   return (
     <section className="px-4 sm:container gap-6 pb-4 sm:pb-8 pt-4 sm:pt-6 md:py-10">
-      Playground
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>${item.price.toFixed(2)}</td>
+              <td>${(item.quantity * item.price).toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   )
 }
